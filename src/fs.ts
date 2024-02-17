87db5ec8bbe7915f251cb99418bb7611fa380f8c
Ganesh B <ganeshsurfs@gmail.com>
2024-02-17 17:35:32 +0530

    [COMMIT] First file commit

---
+++ src/fs.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'node:path'
+import fs from 'node:fs'
+import process from 'node:process'
+
+export interface RunnerContext {
+  programmatic?: boolean
+  hasLock?: boolean
+  cwd?: string
+}
+
+export function getPackageJSON(ctx?: RunnerContext): any {
+  const cwd = ctx?.cwd ?? process.cwd()
+  const path = resolve(cwd, 'package.json')
+
+  if (fs.existsSync(path)) {
+    try {
+      const raw = fs.readFileSync(path, 'utf-8')
+      const data = JSON.parse(raw)
+      return data
+    }
+    catch (e) {
+      if (!ctx?.programmatic) {
+        console.warn('Failed to parse package.json')
+        process.exit(1)
+      }
+
+      throw e
+    }
+  }
+}
